ea1b51f2d44be6fb55ecdc39ccefb0cec61a5583
unknown <dip205pande@gmail.com>
2020-03-02 10:46:49 +0530

    After End of Section 16

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,24 +1,23 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionsTypes';
+import { updatedObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state,action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updatedObject(state,{results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
 
-    switch (action.type) {        
+    switch (action.type) {                
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+            return updatedObject(state,{results: state.results.concat({id: new Date(), value: action.result*2})});
 
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId );
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state,action);
     }
     return state;
 };
